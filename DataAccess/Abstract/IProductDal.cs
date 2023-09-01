1d422f4a2b4103c58061acebde23592e099d0028
Serhat Taştan <serhattastannn@gmail.com>
2023-09-01 11:39:30 +0300

    Ders 8

--- DataAccess/Abstract/IProductDal.cs
+++ DataAccess/Abstract/IProductDal.cs
@@ -10,12 +10,5 @@ namespace DataAccess.Abstract
     public interface IProductDal
     {
 
-        List<Product> GetAll();
-        void Add(Product product);
-        void Delete(Product product);
-        void Update(Product product);
-        List<Product> GetAllByCategory(int categoryId);
-
-
     }
 }
