a3fb3a8bb66b1598717b54b7b79faf492195a220
Serhat Taştan <serhattastannn@gmail.com>
2023-09-05 16:43:26 +0300

    Ders 12

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,5 +1,8 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Business.Abstract;
 using Business.Concrete;
+using Business.DependencyResolvers.Autofac;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 
@@ -8,11 +11,18 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddSingleton<IProductService, ProductManager>();
-builder.Services.AddSingleton<IProductDal, EfProductDal>();
+
+//builder.Services.AddSingleton<IProductService, ProductManager>();         : Autofac ile degistirdik
+//builder.Services.AddSingleton<IProductDal, EfProductDal>();               : Autofac ile degistirdik
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Host.UseServiceProviderFactory(services => new AutofacServiceProviderFactory()).ConfigureContainer<ContainerBuilder>(builder =>
+{ 
+    builder.RegisterModule(new AutofacBusinessModule()); 
+});
 
 var app = builder.Build();
 
